318bb9dec90648a45f0282f5b48dd823e49bd9ea
Marcel Ebert <marcel@satoshipay.io>
2018-10-11 15:15:59 +0200

    add clickListener to layout of WelcomeActivity that launches MainActivity

--- app/src/main/java/de/htwberlin/learningcompanion/WelcomeActivity.kt
+++ app/src/main/java/de/htwberlin/learningcompanion/WelcomeActivity.kt
@@ -1,13 +1,22 @@
 package de.htwberlin.learningcompanion
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import de.htwberlin.learningcompanion.R
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        cl_welcome_activity.setOnClickListener {
+            launchMainActivity()
+        }
+    }
+
+    private fun launchMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java));
     }
 }
