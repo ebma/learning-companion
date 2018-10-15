35f17f4d51491f9dddc62e1561704338cf95611f
Marcel Ebert <marcel@wolfgang-ebert.de>
2018-10-15 18:52:41 +0200

    remove fab from MainActivity; set toolbar textcolor in activity_main layout to white; replace LinearLayout in content_main with ConstraintLayout

--- app/src/main/java/de/htwberlin/learningcompanion/MainActivity.kt
+++ app/src/main/java/de/htwberlin/learningcompanion/MainActivity.kt
@@ -2,7 +2,6 @@ package de.htwberlin.learningcompanion
 
 import android.app.Activity
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import android.view.Menu
 import android.view.MenuItem
 
@@ -15,9 +14,6 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
         setActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
