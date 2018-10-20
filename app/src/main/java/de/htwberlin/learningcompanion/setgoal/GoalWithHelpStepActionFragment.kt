341c46c07e8a7eb851277d1551cd6272d63e17fc
Marcel-Ebert <marcel@wolfgang-ebert.de>
2018-10-20 20:25:42 +0200

    add Button and onClickListener to GoalWithHelpStepActionFragment

--- app/src/main/java/de/htwberlin/learningcompanion/setgoal/GoalWithHelpStepActionFragment.kt
+++ app/src/main/java/de/htwberlin/learningcompanion/setgoal/GoalWithHelpStepActionFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 import de.htwberlin.learningcompanion.R
 
 class GoalNoHelpStepActionFragment : Fragment() {
@@ -15,8 +17,12 @@ class GoalNoHelpStepActionFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_goal_with_help_step_action, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_goal_with_help_step_action, container, false)
+
+        val doneButton = view.findViewById<Button>(R.id.btn_done)
+        doneButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.goalWithHelpStepFieldFragment, null))
+        return view
     }
 
 }
