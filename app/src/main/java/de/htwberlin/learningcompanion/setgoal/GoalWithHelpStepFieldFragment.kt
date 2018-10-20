382861af159fe8b794cd17111f042c95487012bc
Marcel-Ebert <marcel@wolfgang-ebert.de>
2018-10-20 20:26:52 +0200

    add empty GoalWithHelpStepFieldFragment + layout

---
+++ app/src/main/java/de/htwberlin/learningcompanion/setgoal/GoalWithHelpStepFieldFragment.kt
@@ -0,0 +1,23 @@
+package de.htwberlin.learningcompanion.setgoal
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import de.htwberlin.learningcompanion.R
+
+
+class GoalNoHelpStepFieldFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_goal_with_help_step_field, container, false)
+    }
+
+}
