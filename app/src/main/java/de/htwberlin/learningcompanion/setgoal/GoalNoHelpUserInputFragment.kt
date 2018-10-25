5546fc247edf3492c2d5d0a55c8c56d624eead15
Marcel-Ebert <marcel@wolfgang-ebert.de>
2018-10-25 19:59:10 +0200

    add transition to GoalNoHelpUserInputFragment; add ids to editTexts; add arguments to the transition

--- app/src/main/java/de/htwberlin/learningcompanion/setgoal/GoalNoHelpUserInputFragment.kt
+++ app/src/main/java/de/htwberlin/learningcompanion/setgoal/GoalNoHelpUserInputFragment.kt
@@ -6,9 +6,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.google.android.material.textfield.TextInputEditText
 import de.htwberlin.learningcompanion.R
 import java.util.*
 
@@ -21,24 +24,28 @@ class GoalNoHelpUserInputFragment : Fragment() {
     private lateinit var untilAmountEditText: EditText
     private lateinit var forRadioButton: RadioButton
     private lateinit var untilRadioButton: RadioButton
+    private lateinit var doneButton: Button
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         rootView = inflater.inflate(R.layout.fragment_goal_no_help_user_input, container, false)
 
         findViews()
+        addDoneButtonClickListener()
         addRadioButtonClickListeners()
         addTimePickerDialogToUntilAmountEditText()
 
         return rootView
     }
 
-    private fun findViews(){
+    private fun findViews() {
         forAmountEditText = rootView.findViewById(R.id.et_for_amount)
         untilAmountEditText = rootView.findViewById(R.id.et_until_amount)
 
         untilRadioButton = rootView.findViewById(R.id.rb_until)
         forRadioButton = rootView.findViewById(R.id.rb_for)
+
+        doneButton = rootView.findViewById(R.id.btn_done)
     }
 
     private fun addRadioButtonClickListeners() {
@@ -53,7 +60,7 @@ class GoalNoHelpUserInputFragment : Fragment() {
         }
     }
 
-    private fun addTimePickerDialogToUntilAmountEditText(){
+    private fun addTimePickerDialogToUntilAmountEditText() {
         untilAmountEditText.setOnClickListener {
             val mcurrentTime = Calendar.getInstance()
             val hour = mcurrentTime.get(Calendar.HOUR_OF_DAY)
@@ -65,6 +72,29 @@ class GoalNoHelpUserInputFragment : Fragment() {
             mTimePicker.setTitle(getString(R.string.select_time))
             mTimePicker.show()
         }
+    }
+
+    private fun addDoneButtonClickListener() {
+        doneButton.setOnClickListener { navigateToSummaryFragmentWithValues() }
+    }
+
+    private fun navigateToSummaryFragmentWithValues() {
+        val actionEditText = rootView.findViewById<TextInputEditText>(R.id.et_action)
+        val disciplineEditText = rootView.findViewById<TextInputEditText>(R.id.et_discipline)
+        val amountEditText = rootView.findViewById<TextInputEditText>(R.id.et_amount)
+        val mediumEditText = rootView.findViewById<TextInputEditText>(R.id.et_medium)
+
+        val bundle = Bundle()
+        bundle.putString("action", actionEditText.text.toString())
+        bundle.putString("discipline", disciplineEditText.text.toString())
+        bundle.putString("medium", mediumEditText.text.toString())
+        bundle.putString("amount", amountEditText.text.toString())
+        if (untilRadioButton.isEnabled)
+            bundle.putString("timestamp", untilAmountEditText.text.toString())
+        else
+            bundle.putString("timestamp", forAmountEditText.text.toString())
+
+        Navigation.findNavController(rootView).navigate(R.id.action_goalNoHelpUserInputFragment_to_goalSummaryFragment, bundle)
     }
 
 }
