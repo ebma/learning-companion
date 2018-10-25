65e8c412ac0010dfe85debf33e1e5f31c325c476
rockie-xu <klenovyj7list@gmail.com>
2018-10-25 19:35:08 +0200

    nadia, not ready v1.0

---
+++ app/src/main/java/de/htwberlin/learningcompanion/setgoal/GoalWithHelpStepAmountFragment.kt
@@ -0,0 +1,34 @@
+package de.htwberlin.learningcompanion.setgoal
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import de.htwberlin.learningcompanion.R
+
+
+class GoalWithHelpStepFieldFragment : Fragment() {
+
+    private lateinit var rootView : View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        rootView = inflater.inflate(R.layout.fragment_goal_with_help_step_field, container, false)
+        addDoneButtonClickListener()
+        return rootView
+    }
+
+    private fun addDoneButtonClickListener(){
+        val doneButton = rootView.findViewById<Button>(R.id.btn_done)
+        doneButton.setOnClickListener(Navigation.createNavigateOnClickListener(
+                R.id.action_goalWithHelpStepActionFragment_to_goalWithHelpStepFieldFragment, null))
+    }
+
+}
