b78f83fc276a15f354be0b96daefac227ee55999
Marcel-Ebert <marcel@wolfgang-ebert.de>
2018-11-01 19:50:55 +0100

    add MyPlaceFragment + layout

---
+++ app/src/main/java/de/htwberlin/learningcompanion/myplace/MyPlaceFragment.kt
@@ -0,0 +1,20 @@
+package de.htwberlin.learningcompanion.myplace
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import de.htwberlin.learningcompanion.R
+
+class MyPlaceFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_place, container, false)
+    }
+
+
+}
