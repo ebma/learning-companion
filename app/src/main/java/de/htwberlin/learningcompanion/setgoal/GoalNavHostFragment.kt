3dc9002fc909ad30636364248b1fa76ca4b79848
Marcel-Ebert <marcel@wolfgang-ebert.de>
2018-11-06 11:57:39 +0100

    add tab navigation to GoalNoHelpUserInput fragment; add title to GoalNavHostFragment

--- app/src/main/java/de/htwberlin/learningcompanion/setgoal/GoalNavHostFragment.kt
+++ app/src/main/java/de/htwberlin/learningcompanion/setgoal/GoalNavHostFragment.kt
@@ -6,13 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import de.htwberlin.learningcompanion.MainActivity
 import de.htwberlin.learningcompanion.R
 
 class GoalNavHostFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        (activity as MainActivity).supportActionBar?.title = "New goal"
+
         return inflater.inflate(R.layout.fragment_goal_nav_host, container, false)
     }
 
