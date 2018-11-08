2f63614c6ed4c6cdbd15c217573928ec703a282a
Marcel-Ebert <marcel@wolfgang-ebert.de>
2018-11-08 11:11:42 +0100

    add OSMDroid; change GetLocationActivity to use OSM Map services

--- app/src/main/java/de/htwberlin/learningcompanion/myplace/GetLocationActivity.kt
+++ app/src/main/java/de/htwberlin/learningcompanion/myplace/GetLocationActivity.kt
@@ -3,39 +3,32 @@ package de.htwberlin.learningcompanion.myplace
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
-import android.view.View
-import android.widget.FrameLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import de.htwberlin.learningcompanion.R
 import kotlinx.android.synthetic.main.activity_get_location.*
 import kotlinx.android.synthetic.main.content_get_location.*
+import org.jetbrains.anko.ctx
 import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.toast
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+
 
 /**
  * An activity that displays a map showing the place at the device's current location.
  */
-class GetLocationActivity : AppCompatActivity(), OnMapReadyCallback {
-    private var map: GoogleMap? = null
-    private var cameraPosition: CameraPosition? = null
+class GetLocationActivity : AppCompatActivity() {
+    private var map: MapView? = null
 
-    // The entry point to the Fused Location Provider.
-    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
-
-    private val defaultLocation = LatLng(52.504043, 13.393236)
+    private val defaultLocation = GeoPoint(52.504043, 13.393236)
     private var locationPermissionGranted: Boolean = false
 
     // The geographical location where the device is currently located. That is, the last-known
@@ -45,132 +38,88 @@ class GetLocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
     companion object {
         private val TAG = GetLocationActivity::class.java.simpleName
-        private val DEFAULT_ZOOM = 15
+        private val DEFAULT_ZOOM = 15.0
         private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
-
-        // Keys for storing activity state.
-        @JvmStatic
-        private val KEY_CAMERA_POSITION = "camera_position"
-        @JvmStatic
-        private val KEY_LOCATION = "location"
     }
+
+    private lateinit var mLocationOverlay: MyLocationNewOverlay
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Retrieve location and camera position from saved instance state.
-        if (savedInstanceState != null) {
-            lastKnownLocation = savedInstanceState.getParcelable<Location>(KEY_LOCATION)
-            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION)
-//            locationID = savedInstanceState.getLong(ARG_ITEM_ID)
-//            locationName = savedInstanceState.getString(ARG_ITEM_NAME)
-        } else {
-//            locationID = intent.extras.getLong(ARG_ITEM_ID)
-//            locationName = intent.extras.getString(ARG_ITEM_NAME)
-        }
 
         setContentView(R.layout.activity_get_location)
 
         setSupportActionBar(toolbar)
         supportActionBar?.title = "Get location of current place"
 
-        // Construct a FusedLocationProviderClient.
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        Configuration.getInstance().load(applicationContext, PreferenceManager.getDefaultSharedPreferences(ctx))
 
-        // Build the map.
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        if (!locationPermissionGranted) {
+            getLocationPermission()
+        }
+
+        map = findViewById(R.id.map)
+        map?.setTileSource(TileSourceFactory.MAPNIK)
+
+        map?.setBuiltInZoomControls(true)
+        map?.setMultiTouchControls(true)
+
+        map?.controller?.setZoom(DEFAULT_ZOOM)
+        moveCameraToLocation(defaultLocation)
+
+        addLocationOverlay()
+
+        getDeviceLocation()
 
         btn_save_location.onClick {
-
             toast("Location saved!")
         }
 
     }
 
-    /**
-     * Saves the state of the map when the activity is paused.
-     */
-    override fun onSaveInstanceState(outState: Bundle) {
-        if (map != null) {
-            outState.putParcelable(KEY_CAMERA_POSITION, map!!.cameraPosition)
-            outState.putParcelable(KEY_LOCATION, lastKnownLocation)
-//            outState.putLong(ARG_ITEM_ID, locationID)
-//            outState.putString(ARG_ITEM_NAME, locationName)
-            super.onSaveInstanceState(outState)
-        }
+    public override fun onResume() {
+        super.onResume()
+        map?.onResume()
+        mLocationOverlay.enableMyLocation()
     }
 
-    /**
-     * Manipulates the map when it's available.
-     * This callback is triggered when the map is ready to be used.
-     */
-    override fun onMapReady(map: GoogleMap) {
-        this.map = map
+    public override fun onPause() {
+        super.onPause()
+        map?.onPause()
+    }
 
-        // Use a custom info window adapter to handle multiple lines of text in the
-        // info window contents.
-        this.map!!.setInfoWindowAdapter(object : GoogleMap.InfoWindowAdapter {
-
-            override// Return null here, so that getInfoContents() is called next.
-            fun getInfoWindow(arg0: Marker): View? {
-                return null
-            }
-
-            override fun getInfoContents(marker: Marker): View {
-                // Inflate the layouts for the info window, title and snippet.
-                val infoWindow = layoutInflater.inflate(R.layout.custom_info_contents, findViewById(R.id.map) as FrameLayout, false)
-
-                val title = infoWindow.findViewById(R.id.title) as TextView
-                title.text = marker.title
-
-                val snippet = infoWindow.findViewById(R.id.snippet) as TextView
-                snippet.text = marker.snippet
-
-                return infoWindow
-            }
-        })
-
-        // Turn on the My Location layer and the related control on the map.
-        updateLocationUI()
-
-        // Get the current location of the device and set the position of the map.
-        getDeviceLocation()
+    private fun addLocationOverlay() {
+        mLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(applicationContext), map)
+        mLocationOverlay.enableMyLocation()
+        map?.overlays?.add(mLocationOverlay)
     }
 
     /**
      * Gets the current location of the device, and positions the map's camera.
      */
     private fun getDeviceLocation() {
-        /*
-         * Get the best and most recent location of the device, which may be null in rare
-         * cases when a location is not available.
-         */
-        try {
-            if (locationPermissionGranted) {
-                val locationResult = fusedLocationProviderClient!!.getLastLocation()
-                locationResult.addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Set the map's camera position to the current location of the device.
-                        lastKnownLocation = task.result
-                        map!!.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                                LatLng(lastKnownLocation!!.getLatitude(),
-                                        lastKnownLocation!!.getLongitude()), DEFAULT_ZOOM.toFloat()))
-                    } else {
-                        Log.d(TAG, "Current location is null. Using defaults.")
-                        Log.e(TAG, "Exception: %s", task.exception)
-                        map!!.moveCamera(CameraUpdateFactory
-                                .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
-                        map!!.uiSettings.isMyLocationButtonEnabled = false
-                    }
-                }
-            }
-        } catch (e: SecurityException) {
-            Log.e("Exception: %s", e.message)
-        }
+        if (locationPermissionGranted) {
+            val gpsMyLocationProvider = GpsMyLocationProvider(applicationContext)
+            val location = gpsMyLocationProvider.lastKnownLocation
+            Log.d(TAG, "last known location $location")
 
+            gpsMyLocationProvider.startLocationProvider { location, source ->
+                moveCameraToLocation(location)
+                Log.d(TAG, "location changed to ${location?.latitude} ${location?.longitude}")
+            }
+        }
     }
 
+    private fun moveCameraToLocation(location: Location?) {
+        if (location != null) {
+            val geoPoint = GeoPoint(location)
+            map?.controller?.setCenter(geoPoint)
+        }
+    }
+
+    private fun moveCameraToLocation(geoPoint: GeoPoint) {
+        map?.controller?.setCenter(geoPoint)
+    }
 
     /**
      * Prompts the user for permission to use the device location.
@@ -186,7 +135,7 @@ class GetLocationActivity : AppCompatActivity(), OnMapReadyCallback {
                 // sees the explanation, try again to request the permission.
             } else {
                 // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
 
             }
         } else {
@@ -204,32 +153,9 @@ class GetLocationActivity : AppCompatActivity(), OnMapReadyCallback {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     locationPermissionGranted = true
+                    getDeviceLocation()
                 }
             }
         }
-        updateLocationUI()
     }
-
-    /**
-     * Updates the map's UI settings based on whether the user has granted location permission.
-     */
-    private fun updateLocationUI() {
-        if (map == null) {
-            return
-        }
-        try {
-            if (locationPermissionGranted) {
-                map!!.isMyLocationEnabled = true
-                map!!.uiSettings.isMyLocationButtonEnabled = true
-            } else {
-                map!!.isMyLocationEnabled = false
-                map!!.uiSettings.isMyLocationButtonEnabled = false
-                lastKnownLocation = null
-                getLocationPermission()
-            }
-        } catch (e: SecurityException) {
-            Log.e("Exception: %s", e.message)
-        }
-    }
-
 }
