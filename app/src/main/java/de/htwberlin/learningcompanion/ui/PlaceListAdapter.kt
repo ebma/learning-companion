ae3b155b6a3644f6ce1bdd8f59349e48cc7bdcd8
Marcel-Ebert <marcel@wolfgang-ebert.de>
2018-11-08 19:08:14 +0100

    add checkboxes and edit-imagebutton to place_list_item;

--- app/src/main/java/de/htwberlin/learningcompanion/ui/PlaceListAdapter.kt
+++ app/src/main/java/de/htwberlin/learningcompanion/ui/PlaceListAdapter.kt
@@ -5,7 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.FragmentManager
@@ -15,31 +16,28 @@ import de.htwberlin.learningcompanion.R
 import de.htwberlin.learningcompanion.db.PlaceRepository
 import de.htwberlin.learningcompanion.model.Place
 import de.htwberlin.learningcompanion.myplace.details.MyPlaceFragment
-import org.jetbrains.anko.backgroundColor
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class PlaceListAdapter(private val placeDataSet: ArrayList<Place>, val supportFragmentManager: FragmentManager) : RecyclerView.Adapter<PlaceListAdapter.PlaceViewHolder>() {
 
-    class PlaceViewHolder(val rootView: View, private val supportFragmentManager: FragmentManager) : RecyclerView.ViewHolder(rootView), View.OnClickListener {
+    class PlaceViewHolder(val rootView: View, private val supportFragmentManager: FragmentManager) : RecyclerView.ViewHolder(rootView) {
         private lateinit var place: Place
 
         private val tvName: TextView
         private val tvAddress: TextView
         private val ivPlacePreview: ImageView
 
-        private val btnSetCurrent: Button
+        private val btnEdit: ImageButton
+        private val cbSetCurrent: CheckBox
 
         init {
-            rootView.setOnClickListener(this)
             tvName = rootView.findViewById(R.id.tv_name)
             tvAddress = rootView.findViewById(R.id.tv_address)
             ivPlacePreview = rootView.findViewById(R.id.iv_place_preview)
 
-            btnSetCurrent = rootView.findViewById(R.id.btn_set_current)
-        }
-
-        override fun onClick(v: View) {
-            navigateToPlaceDetailFragment()
+            btnEdit = rootView.findViewById(R.id.btn_edit)
+            btnEdit.onClick { navigateToPlaceDetailFragment() }
+            cbSetCurrent = rootView.findViewById(R.id.cb_current_place)
         }
 
         fun bindPlace(place: Place) {
@@ -47,15 +45,9 @@ class PlaceListAdapter(private val placeDataSet: ArrayList<Place>, val supportFr
             tvName.text = place.name
             tvAddress.text = place.addressString
 
-            if (place.currentPlace) {
-                rootView.backgroundColor = itemView.context.resources.getColor(R.color.lightgreen)
-                btnSetCurrent.visibility = View.INVISIBLE
-            } else {
-                rootView.backgroundColor = itemView.context.resources.getColor(R.color.white)
-                btnSetCurrent.visibility = View.VISIBLE
-            }
+            cbSetCurrent.isChecked = place.currentPlace
 
-            btnSetCurrent.onClick {
+            cbSetCurrent.onClick {
                 PlaceRepository.get(itemView.context).setPlaceAsCurrentPlace(place)
             }
 
