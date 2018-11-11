434bb35fdb6ba5468adb126fc516ab92e686add3
nadia <klenovyj7list@gmail.com>
2018-11-11 15:38:01 +0100

    zwischen commit (noch beim GoalListAdapter)

--- app/src/main/java/de/htwberlin/learningcompanion/goaloverview/GoalOverviewFragment.kt
+++ app/src/main/java/de/htwberlin/learningcompanion/goaloverview/GoalOverviewFragment.kt
@@ -14,6 +14,8 @@ import de.htwberlin.learningcompanion.model.Goal
 import de.htwberlin.learningcompanion.setgoal.GoalNavHostFragment
 import de.htwberlin.learningcompanion.ui.GoalListAdapter
 import de.htwberlin.learningcompanion.util.setActivityTitle
+import kotlinx.android.synthetic.main.goal_overview_fragment.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 
 class GoalOverviewFragment : Fragment() {
@@ -38,7 +40,7 @@ class GoalOverviewFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         setActivityTitle(getString(R.string.title_nav_menu_history))
-        //(activity as MainActivity).supportActionBar?.title = getString(R.string.title_nav_menu_goal)
+//        (activity as MainActivity).supportActionBar?.title = getString(R.string.title_nav_menu_goal)
 
 
         viewModel = ViewModelProviders.of(this).get(GoalOverviewViewModel::class.java)
