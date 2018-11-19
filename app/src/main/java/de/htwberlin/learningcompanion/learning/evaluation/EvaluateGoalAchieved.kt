20b88a9ef0b274332171dd72a4ee5b0f9a3d4aa9
Marcel-Ebert <marcel@wolfgang-ebert.de>
2018-11-19 10:43:48 +0100

    add thumb icon' add semitransparent background drawable

--- app/src/main/java/de/htwberlin/learningcompanion/learning/evaluation/EvaluateGoalAchieved.kt
+++ app/src/main/java/de/htwberlin/learningcompanion/learning/evaluation/EvaluateGoalAchieved.kt
@@ -1,16 +1,20 @@
 package de.htwberlin.learningcompanion.learning.evaluation
 
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import de.htwberlin.learningcompanion.R
+import de.htwberlin.learningcompanion.db.PlaceRepository
 
 class EvaluateGoalAchieved : Fragment() {
 
     private lateinit var rootView: View
+    private lateinit var ivPlaceBackground: ImageView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         rootView = inflater.inflate(R.layout.fragment_evaluate_goal_achieved, container, false)
@@ -20,7 +24,20 @@ class EvaluateGoalAchieved : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        ivPlaceBackground = rootView.findViewById(R.id.iv_place_background)
+        setBackgroundPicture()
 
     }
 
+    private fun setBackgroundPicture() {
+        val currentPlace = PlaceRepository.get(context!!).getCurrentPlace()
+
+        if (currentPlace != null) {
+            if (currentPlace.imageUri != null) {
+                val inputStream = activity!!.contentResolver.openInputStream(currentPlace.imageUri)
+                val drawable = Drawable.createFromStream(inputStream, currentPlace.imageUri.toString())
+                ivPlaceBackground.setImageDrawable(drawable)
+            }
+        }
+    }
 }
