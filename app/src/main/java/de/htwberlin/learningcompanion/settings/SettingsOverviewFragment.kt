5377cd5448c4edfad60da2d48024f7924dc537fc
nadia <klenovyj7list@gmail.com>
2018-12-16 14:39:56 +0100

    settings: stand of switches

--- app/src/main/java/de/htwberlin/learningcompanion/settings/SettingsOverviewFragment.kt
+++ app/src/main/java/de/htwberlin/learningcompanion/settings/SettingsOverviewFragment.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Switch
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import de.htwberlin.learningcompanion.R
@@ -14,11 +16,41 @@ class SettingsOverviewFragment : Fragment() {
 
     private lateinit var rootView: View
 
+    private lateinit var switchMicrophone: Switch
+    private lateinit var switchCamera: Switch
+    private lateinit var switchGps: Switch
+
+    private lateinit var userNameEditText: EditText
+    private lateinit var buddyNameEditText: EditText
+    private lateinit var buddyImageEditText: EditText
+    private lateinit var intervalEditText: EditText
+    private lateinit var frequencyEditText: EditText
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         rootView = inflater.inflate(R.layout.fragment_settings_overview, container, false)
 
+        findViews()
+        setSwitchStands()
+
         return rootView
+    }
+
+    private fun findViews() {
+        switchMicrophone = rootView.findViewById(R.id.sw_microphone)
+        switchCamera = rootView.findViewById(R.id.sw_camera)
+        switchGps = rootView.findViewById(R.id.sw_gps)
+        userNameEditText = rootView.findViewById(R.id.et_settings_user_name)
+        buddyNameEditText = rootView.findViewById(R.id.et_settings_buddy_name)
+//        buddyImageEditText = rootView.findViewById(R.id.tv_settings_buddy_image_text)
+        intervalEditText = rootView.findViewById(R.id.et_settings_interval)
+        frequencyEditText = rootView.findViewById(R.id.et_settings_frequency)
+    }
+
+    private fun setSwitchStands() {
+        switchMicrophone.isChecked = hasAudioPermission()
+        switchGps.isChecked = hasLocationPermission()
+        switchCamera.isChecked = hasWritePermission()   // ist es hier, oder ein anderes Befehl?
     }
 
 
@@ -34,78 +66,4 @@ class SettingsOverviewFragment : Fragment() {
     fun hasLocationPermission(): Boolean {
         return ContextCompat.checkSelfPermission(activity!!, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
     }
-
-    /*
-private var action: String? = null
-private var amount: String? = null
-private var discipline: String? = null
-private var medium: String? = null
-private var timestamp: String? = null
-
-
-private lateinit var rootView: View
-private lateinit var goalTextTextView: TextView
-
-override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    rootView = inflater.inflate(R.layout.fragment_goal_summary, container, false)
-
-    initVariablesFromArguments()
-    findViews()
-    setGoalTextInTextView()
-    return rootView
-}
-
-private fun findViews() {
-    goalTextTextView = rootView.findViewById(R.id.tv_goal_text)
-}
-
-private fun initVariablesFromArguments() {
-    amount = arguments?.getString("amount")
-    action = arguments?.getString("action")
-    discipline = arguments?.getString("discipline")
-    medium = arguments?.getString("medium")
-    timestamp = arguments?.getString("timestamp")
-}
-
-private fun setGoalTextInTextView() {
-    val goalText = "${action} ${discipline} ${medium} ${amount} for/until ${timestamp}"
-
-    goalTextTextView.text = goalText
-}
-
-----------------------------
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        rootView = inflater.inflate(R.layout.fragment_goal_overview, container, false)
-
-        updateHeaderLayout()
-        return rootView
-    }
-
-
-    private lateinit var rootView: View
-    private lateinit var sensorHandler: SensorHandler
-
-    private lateinit var btnStart: Button
-    private lateinit var btnQuit: Button
-
-    private lateinit var tvCharlieInfo: TextView
-
-    private val INTERVAL_IN_SECONDS = 5
-    private var permissionToRecordAccepted = false
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        rootView = inflater.inflate(R.layout.fragment_main_screen, container, false)
-        setActivityTitle(getString(R.string.title_nav_menu_main_screen))
-
-        sensorHandler = SensorHandler(activity!!.sensorManager)
-
-        findViews()
-        addClickListeners()
-        showCharlieInfoText()
-        return rootView
-    }
-    */
-
 }
