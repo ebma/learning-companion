1231f5244c28e9018cf4609d725eae7f34c13466
nadia <klenovyj7list@gmail.com>
2019-01-02 12:22:20 +0100

    sharedpreferences still empty

--- app/src/main/java/de/htwberlin/learningcompanion/settings/SettingsOverviewFragment.kt
+++ app/src/main/java/de/htwberlin/learningcompanion/settings/SettingsOverviewFragment.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,10 +13,12 @@ import android.widget.*
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import de.htwberlin.learningcompanion.MainActivity
 import de.htwberlin.learningcompanion.MyPreference
 import de.htwberlin.learningcompanion.R
 import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.support.v4.runOnUiThread
 import org.jetbrains.anko.support.v4.toast
 
 class SettingsOverviewFragment : Fragment() {
@@ -40,6 +43,9 @@ class SettingsOverviewFragment : Fragment() {
 
     private lateinit var btn_settings_save: Button
 
+    private val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+    private var charlieNumberChange = 0
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         rootView = inflater.inflate(R.layout.fragment_settings_overview, container, false)
@@ -54,14 +60,60 @@ class SettingsOverviewFragment : Fragment() {
     }
 
     private fun setDefaultPreferences() {
-        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+//        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
 //        val num = sharedPref.getCharlieNumber()
-        val charlieNum = sharedPref.getInt("CharlieNumber", 0)
-        val userNamePref = sharedPref.getString("UserName", "You")
-        val buddyNamePref = sharedPref.getString("BuddyName", "Charlie")
+        val charlieNum = sharedPref?.getInt("CharlieNumber", 0)
+        val userNamePref = sharedPref?.getString("UserName", "You")
+        val buddyNamePref = sharedPref?.getString("BuddyName", "Charlie")
         rootView.findViewById<EditText>(R.id.input_user_name).hint = userNamePref
         rootView.findViewById<EditText>(R.id.input_buddy_name).hint = buddyNamePref
-        changeName(charlieNum.toString())
+        when(charlieNum) {
+            1 -> changeName("gentle Charlie")
+            2 -> changeName("nerdy Charlie")
+            3 -> changeName("calm Charlie")
+            4 -> changeName("happy Charlie")
+            5 -> changeName("goofy Charlie")
+            else -> {
+                changeName("gentle Charlie")
+            }
+        }
+    }
+
+    private fun savePreferences() {
+        // muss noch ne Bedinguing hinzufügen, wenn die Input Felder nicht leer sind
+        
+        var uname = userNameEditText.text.toString()
+        var bname = buddyNameEditText.text.toString()
+
+        setUserName(uname)
+        setBuddyName(bname)
+        setCharlieName(charlieNumberChange)
+
+        // change hint text in edit-fields
+        rootView.findViewById<EditText>(R.id.input_user_name).hint = uname
+        rootView.findViewById<EditText>(R.id.input_buddy_name).hint = bname
+    }
+
+    private fun setCharlieName(charlieNumber: Int) {
+        val editor = sharedPref!!.edit()
+        editor.putInt("CharlieNumber", charlieNumber)
+        editor.apply()
+    }
+
+    private fun setUserName(userName: String) {
+        val editor = sharedPref!!.edit()
+        editor.putString("UserName", userName)
+        editor.apply()
+    }
+
+    private fun setBuddyName(buddyName: String) {
+        val editor = sharedPref!!.edit()
+        editor.putString("BuddyName", buddyName)
+        editor.apply()
+    }
+
+    private fun changeName(newName: String) {
+        rootView.findViewById<TextView>(R.id.tv_settings_buddy_image_text).text = newName
     }
 
     private fun findViews() {
@@ -85,27 +137,28 @@ class SettingsOverviewFragment : Fragment() {
     private fun addClickListeners() {
         btn_buddyImage1.onClick {
             changeName("gentle Charlie")
+            charlieNumberChange = 1
         }
         btn_buddyImage2.onClick {
             changeName("nerdy Charlie")
+            charlieNumberChange = 2
         }
         btn_buddyImage3.onClick {
             changeName("calm Charlie")
+            charlieNumberChange = 3
         }
         btn_buddyImage4.onClick {
             changeName("happy Charlie")
+            charlieNumberChange = 4
         }
         btn_buddyImage5.onClick {
             changeName("goofy Charlie")
+            charlieNumberChange = 5
         }
         btn_settings_save.onClick {
-            // set new values to preferences
+            savePreferences()
             toast("settings were saved")
         }
-    }
-
-    private fun changeName(newName: String) {
-        rootView.findViewById<TextView>(R.id.tv_settings_buddy_image_text).text = newName
     }
 
     private fun setSwitchStands() {
@@ -113,7 +166,6 @@ class SettingsOverviewFragment : Fragment() {
         switchGps.isChecked = hasLocationPermission()
         switchCamera.isChecked = hasWritePermission()   // ist es hier, oder ein anderes Befehl?
     }
-
 
     // check permissions with
     fun hasWritePermission(): Boolean {
@@ -126,5 +178,14 @@ class SettingsOverviewFragment : Fragment() {
 
     fun hasLocationPermission(): Boolean {
         return ContextCompat.checkSelfPermission(activity!!, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun tintTextInputLayout(layout: TextInputLayout, errorTint: Boolean) {
+        runOnUiThread {
+            if (errorTint)
+                layout.defaultHintTextColor = ColorStateList.valueOf(resources.getColor(android.R.color.holo_red_dark))
+            else
+                layout.defaultHintTextColor = ColorStateList.valueOf(resources.getColor(android.R.color.darker_gray))
+        }
     }
 }
